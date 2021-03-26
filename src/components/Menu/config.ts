157357376ec4c9a848f3d02d658ca2368a550eb8
rumstaking <rumstaking@gmail.com>
2021-03-26 14:03:08 -0300

    Rum AMM init

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,82 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://rumstaking.com/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://rumstaking.com/farms'
+  },
+  {
+    label: 'Staking',
+    icon: 'PoolIcon',
+    href: 'https://rumstaking.com/pools',
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x1d54B2d7e2b8C6F2F3A84914E8720595a579ECe4',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x35f4e1af8d76f33ce8b051b6c15050afab9e83e2',
+      },
+      /* {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/rum-swap',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/rum-swap/',
+      },
+       */
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/rumstaking/",
+      },
+      {
+        label: "Docs",
+        href: "https://docs.rumstaking.com/",
+      },
+      {
+        label: "Blog",
+        href: "https://rumstaking.medium.com",
+      },
+    ],
+  },
+  /* {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://rumstaking.com/files/hackenAudit.pdf',
+  }, */
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x1d54B2d7e2b8C6F2F3A84914E8720595a579ECe4', /* Token address */
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x35f4e1af8d76f33ce8b051b6c15050afab9e83e2'
+}
+
+export default priceContracts
