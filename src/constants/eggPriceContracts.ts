60bd08667d9e7760bc2088a969ce181ba7b77aba
rumstaking <81190147+rumstaking@users.noreply.github.com>
2021-03-29 15:29:56 -0300

    LP address fix

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xCd33351638a9E9f9E507d3448e239063C69C6b99', /* Token address */
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x35f4e1af8d76f33ce8b051b6c15050afab9e83e2'
+  lpAddress: '0x3bb211e05ea6f489d45404d011df7f8ee5c8c2e1'
 }
 
 export default priceContracts
